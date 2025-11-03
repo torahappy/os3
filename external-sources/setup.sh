974073af64808ab351f559038bedfccd0cdb45d1
tora <kawarusosu@zoho.com>
2025-11-04 01:16:31 +0900

    wasm build config

--- external-sources/setup.sh
+++ external-sources/setup.sh
@@ -8,11 +8,12 @@ cd "$SCRIPT_DIR"
 
 . sources
 
+emsdk install 4.0.10
+emsdk activate 4.0.10
+
 if [ ! -d ../external-apps/tesseract ]; then
 
 pushd ./tesseract-$TESSERACT_VERSION
-
-  git clean -dfx
 
   cmake -B build -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/tesseract"
   cmake --build build --config Release -j$NPR
@@ -30,8 +31,6 @@ fi
 if [ ! -d ../external-apps/zlibng-wasm ]; then
 
 pushd zlibng-$ZLIBNG_VERSION
-
-  git clean -dfx
 
   emcmake cmake -B build -DZLIB_COMPAT=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/zlibng-wasm"
   cmake --build build --config Release -j$NPR
@@ -51,8 +50,6 @@ if [ ! -d ../external-apps/png-wasm ]; then
 
 pushd png-$PNG_VERSION
 
-  git clean -dfx
-
   emcmake cmake -B build -DPNG_STATIC=ON -DPNG_SHARED=OFF -DZLIB_INCLUDE_DIR="${SCRIPT_DIR}/../external-apps/zlibng-wasm/include" -DZLIB_LIBRARY="${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib" -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/png-wasm"
   cmake --build build --config Release -j$NPR
 
@@ -70,8 +67,6 @@ if [ ! -d ../external-apps/jpeg-wasm ]; then
 
 pushd jpeg-$JPEG_VERSION
 
-  git clean -dfx
-
   emconfigure ./configure --prefix="${SCRIPT_DIR}/../external-apps/jpeg-wasm"
   make -j$NPR
   make install -j$NPR
@@ -82,13 +77,8 @@ fi
 
 
 if [ ! -d ../external-apps/leptonica-wasm ]; then
-# -- Could NOT find TIFF (missing: TIFF_LIBRARY TIFF_INCLUDE_DIR)
-# -- Could NOT find GIF (missing: GIF_LIBRARY GIF_INCLUDE_DIR) (Required is at least version "5")
-# -- Could NOT find WebP (missing: WebP_DIR)
-# -- Could NOT find OpenJPEG (missing: OpenJPEG_DIR)
+# missing libs: tiff, gif, webp, openjpeg
 pushd leptonica-$LEPTONICA_VERSION
-
-  git clean -dfx
 
   emcmake cmake -B build -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR="${SCRIPT_DIR}/../external-apps/zlibng-wasm/include" -DZLIB_LIBRARY="${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib" -DPNG_PNG_INCLUDE_DIR="${SCRIPT_DIR}/../external-apps/png-wasm/include" -DPNG_LIBRARY="${SCRIPT_DIR}/../external-apps/png-wasm/lib" -DJPEG_INCLUDE_DIR="${SCRIPT_DIR}/../external-apps/jpeg-wasm/include" -DJPEG_LIBRARY="${SCRIPT_DIR}/../external-apps/jpeg-wasm/lib" -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/leptonica-wasm"
   cmake --build build --config Release -j$NPR
@@ -107,9 +97,11 @@ if [ ! -d ../external-apps/tesseract-wasm ]; then
 
 pushd tesseract-$TESSERACT_VERSION
 
-  git clean -dfx
+  cp "${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib/pkgconfig/zlib.pc" "${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib/pkgconfig/ZLIB.pc"
+  cp "${SCRIPT_DIR}/../external-apps/png-wasm/lib/pkgconfig/libpng.pc" "${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib/pkgconfig/PNG.pc"
+  cp "${SCRIPT_DIR}/../external-apps/jpeg-wasm/lib/pkgconfig/libjpeg.pc" "${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib/pkgconfig/JPEG.pc"
 
-  emcmake cmake -B build -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/tesseract-wasm"
+  PKG_CONFIG_PATH="${SCRIPT_DIR}/../external-apps/leptonica-wasm/lib/pkgconfig:${SCRIPT_DIR}/../external-apps/zlibng-wasm/lib/pkgconfig:${SCRIPT_DIR}/../external-apps/png-wasm/lib/pkgconfig:${SCRIPT_DIR}/../external-apps/jpeg-wasm/lib/pkgconfig" emcmake cmake -B build -DCMAKE_INSTALL_PREFIX="${SCRIPT_DIR}/../external-apps/tesseract-wasm"
 
   cmake --build build --config Release -j$NPR
 
@@ -122,8 +114,6 @@ pushd tesseract-$TESSERACT_VERSION
 popd
 
 fi
-
-
 
 if [ ! -f ../external-apps/tesseract/share/tessdata/eng.traineddata ]; then
 
