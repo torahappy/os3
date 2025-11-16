666e9dbcd668d44d5687d8d7762cb61c910f5586
tora <kawarusosu@zoho.com>
2025-11-17 05:09:28 +0900

    rust: supposedly support hidpi

--- os3rust/src/bin/guruguru/main.rs
+++ os3rust/src/bin/guruguru/main.rs
@@ -2,22 +2,21 @@ extern crate sdl2;
 
 use resvg::render;
 use resvg::tiny_skia;
-use resvg::tiny_skia::{PixmapMut};
+use resvg::tiny_skia::PixmapMut;
 use resvg::usvg::fontdb::Database;
 use resvg::usvg::{Transform, Tree};
+use rust_embed::Embed;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
-use rust_embed::Embed;
+use sdl2::sys::SDL_GetWindowSizeInPixels;
+use sdl2::video::Window;
 use std::sync::Arc;
 
 #[derive(Embed)]
 #[folder = "data/"]
 struct Asset;
-
-static SCREEN_WIDTH: u32 = 800;
-static SCREEN_HEIGHT: u32 = 600;
 
 // handle the annoying Rect i32
 macro_rules! rect(
@@ -26,8 +25,26 @@ macro_rules! rect(
     )
 );
 
+
+fn get_window_size_in_pixels (win: &Window) -> (i32, i32) {
+    let mut width: i32 = 0;
+    let mut height: i32 = 0;
+
+    unsafe {
+        SDL_GetWindowSizeInPixels(win.raw(), &mut width, &mut height);
+    }
+    return (width, height)
+}
+
 // Scale fonts to a reasonable size when they're too big (though they might look less smooth)
-fn get_centered_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32) -> Rect {
+fn get_centered_rect(
+    SCREEN_WIDTH: u32,
+    SCREEN_HEIGHT: u32,
+    rect_width: u32,
+    rect_height: u32,
+    cons_width: u32,
+    cons_height: u32,
+) -> Rect {
     let wr = rect_width as f32 / cons_width as f32;
     let hr = rect_height as f32 / cons_height as f32;
 
@@ -63,82 +80,97 @@ fn emscripten_sleep_zero() {
 }
 
 fn run() -> Result<(), String> {
+    let mut SCREEN_WIDTH: u32 = 1000;
+    let mut SCREEN_HEIGHT: u32 = 1000;
+
     println!("load font");
     let fdb = {
         let mut f = Database::new();
-        f.load_font_data(Asset::get("NotoSerifJP-VariableFont_wght.ttf").unwrap().data.to_vec());
-        f.load_font_data(Asset::get("ZenKakuGothicAntique-Regular.ttf").unwrap().data.to_vec());
+        f.load_font_data(
+            Asset::get("NotoSerifJP-VariableFont_wght.ttf")
+                .unwrap()
+                .data
+                .to_vec(),
+        );
+        f.load_font_data(
+            Asset::get("ZenKakuGothicAntique-Regular.ttf")
+                .unwrap()
+                .data
+                .to_vec(),
+        );
         f
     };
 
     println!("load svg");
     let svg_data = Asset::get("example.svg").unwrap().data;
 
-    println!("make tree");
-    let t = Tree::from_data(
-        &svg_data,
-        &resvg::usvg::Options {
-            resources_dir: None,
-            dpi: 300.0,
-            font_family: "Noto Serif JP".to_string(),
-            font_size: 16.0,
-            languages: vec!["en".to_string()],
-            fontdb: Arc::new(fdb),
-            ..Default::default()
-        },
-    )
-    .unwrap();
-
-    println!("draw tree");
-    let mut binding = vec![0; 1000 * 1000 * 4];
-        let mut p = PixmapMut::from_bytes(binding.as_mut_slice(), 1000, 1000).unwrap();
-
-        p.fill(tiny_skia::Color::from_rgba8(255, 255, 255, 255));
-        render(
-            &t,
-            Transform {
-                sx: 1.0,
-                kx: 0.0,
-                ky: 0.0,
-                sy: 1.0,
-                tx: 0.0,
-                ty: 0.0,
-            },
-            &mut p,
-        );
 
     let sdl_context = sdl2::init()?;
     let video_subsys = sdl_context.video()?;
     // let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
 
     let window = video_subsys
-        .window("SDL2_TTF Example", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .window("ぐるぐるテスト", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .resizable()
         .opengl()
+        .allow_highdpi()
         .build()
         .map_err(|e| e.to_string())?;
+
+    let (raw_w, raw_h) = get_window_size_in_pixels(&window);
+
+    println!("Raw Window Size: {} {}", raw_w, raw_h);
+
+    let density: f32 = (raw_w as f32) / (SCREEN_WIDTH as f32);
+
+    println!("Density: {}", density);
+
+    println!("make tree");
+    let t = Tree::from_data(
+        &svg_data,
+        &resvg::usvg::Options {
+            resources_dir: None,
+            dpi: 72.0 * density,
+            font_family: "Noto Serif JP".to_string(),
+            font_size: 16.0,
+            languages: vec!["en".to_string()],
+            fontdb: Arc::new(fdb),
+            text_rendering: resvg::usvg::TextRendering::GeometricPrecision,
+            shape_rendering: resvg::usvg::ShapeRendering::GeometricPrecision,
+            image_rendering: resvg::usvg::ImageRendering::OptimizeQuality,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    println!("draw tree");
+    let mut binding = vec![0; (raw_w as usize) * (raw_h as usize) * 4];
+    let mut p = PixmapMut::from_bytes(binding.as_mut_slice(), raw_w as u32, raw_h as u32).unwrap();
+
+    p.fill(tiny_skia::Color::from_rgba8(255, 255, 255, 255));
+    render(
+        &t,
+        Transform {
+            sx: 1.0,
+            kx: 0.0,
+            ky: 0.0,
+            sy: 1.0,
+            tx: 0.0,
+            ty: 0.0,
+        },
+        &mut p,
+    );
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let texture_creator = canvas.texture_creator();
 
-    // Load a font
-    // let mut font = ttf_context.load_font(font_path, 60)?;
-    // font.set_style(sdl2::ttf::FontStyle::NORMAL);
-
-    // render a surface, and convert it to a texture bound to the canvas
-    //let surface = font
-    //    .render("あいうえお")
-    //    .blended(Color::RGBA(255, 0, 0, 255))
-    //    .map_err(|e| e.to_string())?;
-    //let texture = texture_creator
-    //    .create_texture_from_surface(&surface)
-    //    .map_err(|e| e.to_string())?;
-
     let mut texture2 = texture_creator
-        .create_texture_streaming(PixelFormatEnum::ABGR8888, 1000, 1000)
+        .create_texture_streaming(PixelFormatEnum::ABGR8888, SCREEN_WIDTH, SCREEN_HEIGHT)
         .unwrap();
-    texture2.update(None, &binding.as_slice(), 1000*4).unwrap();
+    texture2
+        .update(None, &binding.as_slice(), (raw_w as usize) * 4)
+        .unwrap();
 
     canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
     canvas.clear();
@@ -146,19 +178,19 @@ fn run() -> Result<(), String> {
     // let TextureQuery { width, height, .. } = texture.query();
 
     'mainloop: loop {
-    // If the example text is too big for the screen, downscale it (and center irregardless)
-    let padding = 64;
-    //let target = get_centered_rect(
-    //    width,
-    //    height,
-    //    SCREEN_WIDTH - padding,
-    //    SCREEN_HEIGHT - padding,
-    //);
+        // If the example text is too big for the screen, downscale it (and center irregardless)
+        let padding = 64;
+        //let target = get_centered_rect(
+        //    width,
+        //    height,
+        //    SCREEN_WIDTH - padding,
+        //    SCREEN_HEIGHT - padding,
+        //);
 
-    //canvas.copy(&texture, None, Some(target))?;
-    //canvas.present();
-    canvas.copy(&texture2, None, None)?;
-    canvas.present();
+        //canvas.copy(&texture, None, Some(target))?;
+        //canvas.present();
+        canvas.copy(&texture2, None, None)?;
+        canvas.present();
         for event in sdl_context.event_pump()?.poll_iter() {
             match event {
                 Event::KeyDown {
