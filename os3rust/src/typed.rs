5a20843f6d9ab6d6a0ef1178ae82736f6f6c0bc5
tora <kawarusosu@zoho.com>
2025-12-21 06:43:20 +0900

    rust: impl parallel combine

--- os3rust/src/typed.rs
+++ os3rust/src/typed.rs
@@ -1,7 +1,6 @@
 /// SF takes some input, then combines them with its own inner variable and updates both the inner
 /// variable and the output. It also holds the initial inner variable and the initial output, to
 /// make itself well-defined.
-/// The lifetime `'a` specifies how long the rust functions in all the SF live.
 /// A = input type, B = output type, C = inner state type
 pub fn scan_option<A, B: Clone, C, F: Fn(&A, &C) -> Option<(B, C)>>(
     the_func: &F,
@@ -33,7 +32,7 @@ pub fn scan<'a, A, B: Clone, C, F: Fn(&A, &B) -> B + 'a>(
 pub fn lift_simple<'a, A, B, F: Fn(&A) -> B + 'a>(
     base_func: &F,
 ) -> (impl Fn(&A, &()) -> Option<(B, ())>, impl Fn(&A) -> B, ()) {
-    return (move |a, _| Some((base_func(a), ())), |x| base_func(x), ());
+    return (|a, _| Some((base_func(a), ())), |x| base_func(x), ());
 }
 
 /// pass through the second element as-is. process the first element with the given SF.
@@ -57,7 +56,7 @@ pub fn process_first<A, B, C, D: Clone, F: Fn(&A, &C) -> Option<(B, C)>, G: Fn(&
 
 /// pass through the first element as-is. process the second element with the given SF.
 pub fn process_second<A, B, C, D: Clone, F: Fn(&A, &C) -> Option<(B, C)>, G: Fn(&A) -> B>(
-    base_sf: (&F, G, C),
+    base_sf: (&F, &G, C),
 ) -> (
     impl Fn(&(D, A), &C) -> Option<((D, B), C)>,
     impl Fn(&(D, A)) -> (D, B),
@@ -74,43 +73,53 @@ pub fn process_second<A, B, C, D: Clone, F: Fn(&A, &C) -> Option<(B, C)>, G: Fn(
     );
 }
 
-///
+/// combine two SF
 pub fn combine_parallel<
     A,
-    B,
-    C,
+    B: Clone,
+    C: Clone,
     D,
-    E,
-    F,
+    E: Clone,
+    F: Clone,
     S: Fn(&A, &C) -> Option<(B, C)>,
     T: Fn(&D, &F) -> Option<(E, F)>,
+    U: Fn(&A, &C) -> Option<(B, C)>,
+    V: Fn(&D, &F) -> Option<(E, F)>,
+    W: Fn(&A) -> B,
+    X: Fn(&D) -> E
 >(
-    base_sf_1: (impl Fn(&A, &C) -> Option<(B, C)>, impl Fn(&A) -> B, C),
-    base_sf_2: (impl Fn(&D, &F) -> Option<(E, F)>, impl Fn(&D) -> E, F),
+    base_sf_1: (&U, &W, C),
+    base_sf_2: (&V, &X, F),
 ) -> (
-    impl Fn(&(A, D), &(C, F)) -> Option<((B, E), (C, F))>,
+    impl Fn(&(A, D), &(C, F, Option<B>, Option<E>)) -> Option<((B, E), (C, F, Option<B>, Option<E>))>,
     impl Fn(&(A, D)) -> (B, E),
-    (C, F),
+    (C, F, Option<B>, Option<E>),
 ) {
     let (f_1, init_o_1, init_s_1) = base_sf_1;
     let (f_2, init_o_2, init_s_2) = base_sf_2;
     (
-        move |(a, d), (c, f)| {
-            let x = f_1(a, c);
-            let y = f_2(d, f);
+        move |(a, d), (c_pre, f_pre, last_1, last_2)| {
+            let x = f_1(a, c_pre);
+            let y = f_2(d, f_pre);
             if x.is_some() && y.is_some() {
                 let (b, c) = x.unwrap();
                 let (e, f) = y.unwrap();
-                Some(((b, e), (c, f)))
+                Some(((b.clone(), e.clone()), (c, f, Some(b), Some(e))))
             } else if x.is_none() && y.is_some() {
-                todo!()
+                let (e, f) = y.unwrap();
+                let b = last_1.clone().unwrap();
+                let c = c_pre;
+                Some(((b.clone(), e.clone()), (c.clone(), f, Some(b), Some(e))))
             } else if x.is_some() && y.is_none() {
-                todo!()
+                let (b, c) = x.unwrap();
+                let e = last_2.clone().unwrap();
+                let f = f_pre;
+                Some(((b.clone(), e.clone()), (c, f.clone(), Some(b), Some(e))))
             } else {
                 None
             }
         },
         move |(a, d)| (init_o_1(a), init_o_2(d)),
-        (init_s_1, init_s_2),
+        (init_s_1, init_s_2, None, None),
     )
 }
