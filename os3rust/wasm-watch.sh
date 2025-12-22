247a5e771410f15594ef2eddb0f7e87907a413d8
tora <kawarusosu@zoho.com>
2025-12-22 23:01:04 +0900

    rust: build tools: fix

--- os3rust/wasm-watch.sh
+++ os3rust/wasm-watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chokidar 'src/**/*' 'build.rs' --initial -c "cargo build --profile=release --target=wasm32-unknown-unknown --bin $1; mkdir ./wasm/$1; cp ./target/wasm32-unknown-unknown/release/$1.wasm ./wasm/$1;"
+chokidar 'src/**/*' 'build.rs' --initial -c "cargo build --profile=release --target=wasm32-unknown-unknown --bin $1; mkdir ./wasm/$1; cp ./target/wasm32-unknown-unknown/release/$1.wasm ./wasm/$1;$HOME/.cargo/bin/wasm-bindgen --target web --out-dir ./wasm/$1 ./wasm/$1/$1.wasm"
