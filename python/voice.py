2bd31553ceb55898f957acff26a3df256658f6f6
tora <kawarusosu@zoho.com>
2026-01-11 12:14:12 +0900

    microphone prep

--- python/voice.py
+++ python/voice.py
@@ -8,13 +8,12 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 import scipy.io.wavfile as wavfile
 import sounddevice as sd
+from matplotlib.animation import FuncAnimation
 
 # プリエンファシス(高域強調)
 def preEmphasis(wave, p=0.97):
     # 係数 (1.0, -p) のFIRフィルタを作成
     return scipy.signal.lfilter([1.0, -p], 1, wave)
-
-boin_list = ["a","i","u","e","o"]
 
 def my_autocorr(x, order):
   N = len(x)
@@ -38,20 +37,22 @@ def my_levinson(signal, order=10):
     A = U+V
     e = (1 - lam**2) * e
 
-  A = -A
-  A[0] = 1.0
-
   return A, e
 
-fs =44100
-sd.default.samplerate = fs
-sd.default.channels = 1
-sd.default.device = 0
-sd.default.dtype = 'float32'
+fs = 48000
 
 dl = sd.query_devices()
 for dev in dl:
-    pass
+    print(dev)
+    if 'hw:0,0' in dev['name']:
+        print("found: ", dev)
+        my_idx = dev['index']
+        break
+
+sd.default.samplerate = fs
+sd.default.channels = 1
+sd.default.device = my_idx
+sd.default.dtype = 'int16'
 
 ms = 40
 
@@ -60,21 +61,20 @@ window = int(fs / 1000 * ms)
 
 # タップ数、サンプルレート、チャンネル数
 
-# データの読み込み, 音素ごとに辞書化
-for i in range(1,100,1):
-    v = sd.rec(window)
+arcoefs, coeffs_2 = [None, None]
 
+fig, ax = plt.subplots()
+def update(frame):
+    ax.clear()
+    v = sd.rec(window, samplerate=fs).mean(axis=1)
+    # print(v.shape)
     sd.wait()
 
     wav_list = []
-
     
     # LPC係数を求める(lpcの次数は要調整)
-    lpcOrder = 32
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
+    lpcOrder = 20
     #センター部分を使う
-    cuttime = 0.04
     voice_data = v
     #正規化
     voice_data = voice_data/abs(voice_data).max()
@@ -84,36 +84,39 @@ for i in range(1,100,1):
     #ハミング窓
     hammingWindow = np.hamming(len(voice_data))
     voice_data = voice_data * hammingWindow    
-    sigma_v, arcoefs, pacf, sigma, phi = levinson_durbin(voice_data, lpcOrder)
-    coeffs_2, error_2 = my_levinson(voice_data, lpcOrder)
-    print("Variance 1: " + str(sigma_v))
-    print("Variance 2: " + str(error_2))
-    print("Coeffs 1: " + str(arcoefs))
-    print("Coeffs 2: " + str(coeffs_2))
 
-
-
-
-    """
-    # LPC係数の振幅スペクトルを求める
     sample = len(voice_data)
+
+    sigma_v, arcoefs, pacf, sigma, phi = levinson_durbin(voice_data, lpcOrder)
+    coeffs_1 = np.hstack((1, arcoefs))
+    error_1 = sigma_v * sample
+    coeffs_2, error_2 = my_levinson(voice_data, lpcOrder)
+
+    #print("Variance 1: " + str(error_1))
+    #print("Variance 2: " + str(error_2))
+    #print("Coeffs 1: " + str(coeffs_1))
+    #print("Coeffs 2: " + str(coeffs_2))
+    # LPC係数の振幅スペクトルを求める
     fscale = np.fft.fftfreq(sample, d = 1.0 / fs)[:sample//2]
     # オリジナル信号の対数スペクトル
     spec = np.abs(fft(voice_data, sample))
     logspec = 20 * np.log10(spec)
     ax.plot(fscale, logspec[:sample//2])
     # LPC対数スペクトル
-    w, h = scipy.signal.freqz(np.sqrt(e), a, sample, "whole")
+    w, h = scipy.signal.freqz(np.sqrt(error_1), coeffs_1, sample, "whole")
     lpcspec = np.abs(h)
     loglpcspec = 20 * np.log10(lpcspec)
     #出力をプロットしてみて出力
     ax.plot(fscale, loglpcspec[:sample//2], "r", linewidth=2)
     maxId = scipy.signal.argrelmax(loglpcspec[:sample//2],order=3)
-    maxId = maxId[0]
+    if len(maxId[0]) > 1:
+        maxId = maxId[0]
     #とりあえず4つ分ぐらいのフォルマントの位置を出力
-    ax.axvline(fscale[maxId[0]], ls = "--", color = "navy")
-    ax.axvline(fscale[maxId[1]], ls = "--", color = "navy")
-    ax.axvline(fscale[maxId[2]], ls = "--", color = "navy")
-    ax.axvline(fscale[maxId[3]], ls = "--", color = "navy")
+        ax.axvline(fscale[maxId[0]], ls = "--", color = "navy")
+        ax.axvline(fscale[maxId[1]], ls = "--", color = "navy")
     plt.show()
-    """
+    return
+
+anim = FuncAnimation(fig, update, frames = None, cache_frame_data=False, interval=0)
+plt.show()
+
