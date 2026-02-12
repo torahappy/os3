31ee8ac82aaacf085a0c5caffb989d8fc66031bf
tora <kawarusosu@zoho.com>
2026-02-12 23:41:22 +0900

    shimbun: fetch resources

--- os3yew/auto-fetch.py
+++ os3yew/auto-fetch.py
@@ -31,18 +31,26 @@ def get_notes() -> List[Tuple[str, str]]:
     return [(a, b) for (a, b, c) in notes if c == 0]
 
 if __name__ == '__main__':
-    assets_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets', 'texts')
-    shutil.rmtree(assets_dir)
-    os.makedirs(assets_dir)
+    assets_dir_texts = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets', 'texts')
+    shutil.rmtree(assets_dir_texts)
+    os.makedirs(assets_dir_texts)
+    assets_dir_images = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets', 'images')
+    shutil.rmtree(assets_dir_images)
+    os.makedirs(assets_dir_images)
+
     notes = get_notes()
     for note_title, note_body in notes:
         if note_title.startswith('!!!'):
             continue
+
         # Create a safe filename by replacing invalid characters
         safe_filename = "".join(c if regex.match(r'[\w\d\p{Han}\p{Hiragana}\p{Katakana}「」]', c) else '_' for c in note_title)
         if safe_filename == '':
             continue
         safe_filename += '.txt'
+
         # Write the note content to a file
-        with open(os.path.join(assets_dir, safe_filename), 'w', encoding='utf-8') as f:
+        with open(os.path.join(assets_dir_texts, safe_filename), 'w', encoding='utf-8') as f:
             f.write(note_body)
+
+        
