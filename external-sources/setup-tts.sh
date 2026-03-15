ef693f54b2101a87cac3920e5bc1376859dcb7e5
tora <kawarusosu@zoho.com>
2026-03-15 18:47:45 +0900

    build: add setup script

--- external-sources/setup-tts.sh
+++ external-sources/setup-tts.sh
@@ -11,7 +11,20 @@ cd "$SCRIPT_DIR"
 
 . sources
 
-if [ ! -d ../external-apps/ ]; then
+if [ ! -d ../external-apps/open_jtalk ]; then
+  pushd ./hts_engine_api-$HTS_ENGINE_API_VERSION
+  ./configure --prefix="$SCRIPT_DIR/../external-apps/open_jtalk"
+  make -j$NPR
+  make install
+  popd
 
+  pushd ./open_jtalk-$OPEN_JTALK_VERSION/src
+  mkdir build
+  pushd build
+  cmake -DBUILD_PROGRAMS=1 -DCMAKE_INSTALL_PREFIX="$SCRIPT_DIR/../external-apps/open_jtalk" ..
+  make -j$NPR
+  make install
+  popd
+  popd
 fi
 
