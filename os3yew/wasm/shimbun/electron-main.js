d2da4e0a787e816673aa046dacd19ec9e768cdad
tora <kawarusosu@zoho.com>
2026-03-25 02:58:44 +0900

    shimbun: add electron forge

---
+++ os3yew/wasm/shimbun/electron-main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true
+  })
+
+  win.loadURL('http://localhost:3000/select.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
