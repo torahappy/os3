9bda5bb3914dddedc802d814afd8fde115562883
tora <kawarusosu@zoho.com>
2026-03-25 06:04:19 +0900

    packaging: fix scripts

--- packaging/shimbun/do_all.sh
+++ packaging/shimbun/do_all.sh
@@ -5,6 +5,6 @@ yarn electron-forge make -a x64 -p linux
 yarn electron-forge make -a ia32 -p linux
 yarn electron-forge make -a arm64 -p darwin
 yarn electron-forge make -a x64 -p darwin
-yarn electron-forge make -a arm64 -p windows
-yarn electron-forge make -a x64 -p windows
-yarn electron-forge make -a ia32 -p windows
+yarn electron-forge make -a arm64 -p win32
+yarn electron-forge make -a x64 -p win32
+yarn electron-forge make -a ia32 -p win32
