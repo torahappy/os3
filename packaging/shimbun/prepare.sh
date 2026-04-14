6822240d0e0139bba07bbf5a8838124ec52aadfc
tora <kawarusosu@zoho.com>
2026-04-14 14:05:37 +0900

    packaging: update prepare.sh

--- packaging/shimbun/prepare.sh
+++ packaging/shimbun/prepare.sh
@@ -2,6 +2,14 @@
 
 cd "$(dirname "$0")"
 
+BUILD_NATIVE=0 BUILD_WASM=1 ../../external-sources/setup-tts.sh
+../../build_scripts/wasm-once.sh os3yew shimbun
+../../os3yew/install-wasm-binaries.sh
+
+pushd ../../os3yew/wasm/shimbun/
+NODE_ENV=production yarn
+popd
+
 rm -rf ./data
 mkdir ./data
 cp -r ../../os3yew/wasm/shimbun/* data/
