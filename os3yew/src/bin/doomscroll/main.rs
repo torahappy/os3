2f7df8ead4e0c6cc847d3e6e83729f0d32a3f36a
tora <kawarusosu@zoho.com>
2026-06-04 23:21:03 +0900

    doomscroll: impl smartphone scroll

--- os3yew/src/bin/doomscroll/main.rs
+++ os3yew/src/bin/doomscroll/main.rs
@@ -5,7 +5,31 @@ use yew::prelude::*;
 #[component]
 fn App() -> Html {
     html! {
-        <div>
+        <div class="root">
+            <img src="assets/komagire/tushin.webp" />
+            <img src="assets/komagire/tushin.webp" />
+            <img src="assets/komagire/tushin.webp" />
+            <img src="assets/komagire/tushin.webp" />
+            <img src="assets/komagire/tushin.webp" />
+            <img src="assets/komagire/tushin.webp" />
+            <div class="komagire-wrap">
+                <img src="assets/komagire/bt1-1.webp" />
+                <img src="assets/komagire/bt1-2.webp" />
+                <img src="assets/komagire/bt1-3.webp" />
+            </div>
+            <img src="assets/komagire/tushin.webp" />
+            <div class="komagire-wrap">
+                <img src="assets/komagire/bt2-1.webp" />
+                <img src="assets/komagire/bt2-2.webp" />
+                <img src="assets/komagire/bt2-3.webp" />
+            </div>
+            <img src="assets/komagire/tushin.webp" />
+            <div class="komagire-wrap">
+                <img src="assets/komagire/s1.webp" />
+                <img src="assets/komagire/s2.webp" />
+                <img src="assets/komagire/s3.webp" />
+            </div>
+            <img src="assets/komagire/tushin.webp" />
         </div>
     }
 }
