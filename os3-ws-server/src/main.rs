1cb9d24561e55928e58df228a60feefb1c3feaba
tora <kawarusosu@zoho.com>
2026-06-08 16:59:30 +0900

    doomscroll: update ci and change paths and host IP

--- os3-ws-server/src/main.rs
+++ os3-ws-server/src/main.rs
@@ -304,11 +304,10 @@ async fn main() {
     tokio::spawn(async move { inactivity_checker(state_clone).await });
 
     let app = Router::new()
-        .route("/ws", get(ws_handler))
+        .route("/doomscroll_web/ws", get(ws_handler))
         .fallback_service(ServeDir::new(Path::new("../os3yew/wasm/doomscroll/")))
         .with_state(shared_state);
 
-    // Listen on 0.0.0.0:3000
-    axum::serve(tokio::net::TcpListener::bind("127.0.0.1:6543").await.unwrap(), app.into_make_service()).await.unwrap();
+    axum::serve(tokio::net::TcpListener::bind("0.0.0.0:6543").await.unwrap(), app.into_make_service()).await.unwrap();
 }
 
